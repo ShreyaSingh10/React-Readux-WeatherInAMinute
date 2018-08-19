356483b9f84a9df390d8861dbc5262f40f765986
ShreyaSingh10 <shreyasssingh10gmail.com>
2018-08-19 15:28:30 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import store from './store/';
+import Report from './Report';
+import {Provider} from 'react-redux';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Provider store={store}>
+        <Report/>
+      </Provider>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import {createStore} from 'redux';
+
+const initialState ={
+	weather_report:''
+};
+
+
+const reducer = (state=initialState, action) =>{
+	console.log('reducer', action);
+
+	switch(action.type){
+		case 'GET_WEATHER':
+			return Object.assign({}, state, { weather_report: action.weather_report });
+		default:
+			return state;
+	}
+}
+
+const store = createStore(reducer);
+
+export default store;
